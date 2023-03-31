70d129ed86001183bca348233118917186801e0e
Carson Mifsud <113727119+carsonfmm@users.noreply.github.com>
2023-03-31 17:00:00 -0400

    Delete LabExercise1/src/test/java/LabExerciseOne directory

--- LabExercise1/src/test/java/LabExerciseOne/Test.java
+++
@@ -1,5 +0,0 @@
-package LabExerciseOne;
-
-public @interface Test {
-
-}
--- LabExercise1/src/test/java/LabExerciseOne/src/test/java/LabExerciseOne/App.java
+++
@@ -1,9 +0,0 @@
-package LabExerciseOne.src.test.java.LabExerciseOne;
-
-public class App {
-
-	public String getGreeting() {
-		return null;
-	}
-
-}
